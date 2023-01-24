da740f74b5d567a1ecd8f37d690798fdb0145f85
Sprinkle Tang <65575548+pntang@users.noreply.github.com>
2023-01-24 20:26:54 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,5 @@
 apt update && apt install -y wget
-wget https://github.com/fatedier/frp/releases/download/v0.16.0/frp_0.16.0_linux_amd64.tar.gz
-tar -zxf frp_0.16.0_linux_amd64.tar.gz
-mv frp_0.16.0_linux_amd64 /var/frp
-rm -rf /var/frp/frps.ini
-mv frps.ini /var/frp/frps.ini
-./var/frp/frps -c /var/frp/frps.ini
+wget https://github.com/fatedier/frp/releases/download/v0.46.1/frp_0.46.1_linux_amd64.tar.gz
+tar -zxf frp_0.46.1_linux_amd64.tar.gz
+chmod +x ./frps
+./frps -c ./frps.ini
